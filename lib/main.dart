2a7b1af6e2924d4fdb3b8a049dff4d04a1525da0
Vee <avhadpriyanka88@gmail.com>
2021-04-04 00:25:30 +0530

    splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-void main() {
+import 'package:flutter/services.dart';
+import 'camera.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyApp());
 }
 
@@ -8,51 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Splash Screen',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
-      home: MyHomePage(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 3),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                SecondScreen()
-            )
-        )
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        color: Colors.blueGrey,
-        child:Image(
-          image: AssetImage("images/assistance_logo.png"),
-        )
-    );
-  }
-}
-class SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:Text("Assistance")),
-      body: Center(
-          child:Text("Home page",textScaleFactor: 2,)
-      ),
+      home: CameraScreen(),
     );
   }
 }
